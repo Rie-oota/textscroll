6c50756566c9edfdaf71ffbf8e3cb25f8644910f
Yoshida <sunnri257@gmail.com>
2025-07-06 13:53:49 +0900

    修正版

--- js/textscroll.js
+++ js/textscroll.js
@@ -1,49 +1,41 @@
 // グローバル変数をboolean型で定義
-const scrollFlag = true;
+let scrollFlag = true;
 
 // スクロール操作した時の処理を指定
 $(window).on('scroll', function () {
-  // scrollFlagがtrueであれば処理を実行
-  if (scrollFlag) {
-    // クラス名【inview】が付与されている要素に対して処理を実行
-    $('.inview').each(function() {
-      // ブラウザの最上部から要素までの位置を取得
-      const targetPosition = $(this).offset().top;
-      // ブラウザの最上部からのスクロール量を取得
-      const scroll = $(window).scrollTop();
-      // ウィンドウの高さを取得
-      const windowHeight = $(window).height();
-      // ブラウザの最上部からのスクロール量 > 要素の位置 - ウィンドウの高さ の場合
-      if (scroll > targetPosition - windowHeight) {
-        // クラス名【show】を追加
-        $(this).addClass('show');
-      }
-    });
-  }else{
-    // scrollFlagがfalseであればアーリーリターン
-    return false;
+  // scrollFlagがfalseであれば処理を抜ける
+  if (scrollFlag = false) {
+    return;
   }
+  // クラス名【inview】が付与されている要素に対して処理を実行
+  $('.inview').each(function() {
+    // ブラウザの最上部から要素までの位置を取得
+    const targetPosition = $(this).offset().top;
+    // ブラウザの最上部からのスクロール量を取得
+    const scroll = $(window).scrollTop();
+    // ウィンドウの高さを取得
+    const windowHeight = $(window).height();
+    // ブラウザの最上部からのスクロール量 > 要素の位置 - ウィンドウの高さ の場合
+    if (scroll > targetPosition - windowHeight) {
+      // クラス名【show】を追加
+      $(this).addClass('show');
+    }
+  });
 });
 
 // 画面を読み込んだ時の処理を指定
 $(window).on('load', function () {
-  // if判定で処理を分岐する
-  if (scrollFlag) {
-    // ページトップへ移動する
-    $('html,body').animate({
-      scrollTop: 0
-      // アニメーション完了後に実行する処理
-    },function() {
-      // クラス名【show】を外す
-      $('.inview').each(function() {
-        $(this).removeClass('show');
-      });
-      scrollFlag = true;
+  // ページトップへ移動する
+  $('html,body').animate({
+    scrollTop: 0
+  // アニメーション完了後に実行する処理
+  },function() {
+    // クラス名【show】を外す
+    $('.inview').each(function() {
+      $(this).removeClass('show');
     });
-  }else{
-    // スクロールイベント内の処理を実行しないようにする
-    $(window).off('scroll');
-  }
+  });
+  scrollFlag = true;
 });
 
 // ホバーした時の処理を指定
